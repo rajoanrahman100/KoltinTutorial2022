17ac498077565be793464e8ba06ad14aed9b983f
sahik1234 <sahikahtesam1994@gmail.com>
2022-10-23 08:09:03 +0600

    Singleton object and data class

---
+++ src/main/kotlin/data_class/data_class.kt
@@ -0,0 +1,18 @@
+package data_class
+
+
+data class Employee(var name:String,var address:String){ //Primary Constructor
+
+}
+
+fun main() {
+    var emp=Employee("Rifat","Saydabad")
+    var emp2=Employee("Rifat2","Saydabad")
+
+    println(emp)
+    println(emp==emp2)
+
+}
+
+//Todo:If we want to compare two objet to see equal or not, we can put data before class name
+//Todo: If we want to print the primary constructor value of a class, we can put data and print the object of that class
---
+++ src/main/kotlin/data_class/object_keyword.kt
@@ -0,0 +1,20 @@
+package data_class
+
+
+data class Student(var name: String, var id: Int)
+
+//Todo:To convert a class into singleton class, object keyword is used
+//Object keyword insured that,only one object can be created for a particular class
+object AbcInstitution {
+    var students = arrayListOf<Student>()
+}
+
+fun main() {
+    AbcInstitution.students.add(Student("Rifat", 27))
+    AbcInstitution.students.add(Student("Bristy", 23))
+
+    for (i in AbcInstitution.students) {
+        println(i)
+    }
+
+}
