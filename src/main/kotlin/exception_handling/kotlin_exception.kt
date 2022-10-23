8db7a35d4863b20d4201111a4f62e37740a0027a
sahik1234 <sahikahtesam1994@gmail.com>
2022-10-23 19:42:44 +0600

    Exception Added

--- src/main/kotlin/exception_handling/kotlin_exception.kt
+++ src/main/kotlin/exception_handling/kotlin_exception.kt
@@ -5,7 +5,7 @@ import java.lang.Exception
 // Todo: Two Types of Exception
 /**
  * Checked:  Exceptions that are typically set on methods and checked at the compile time,
-for example IOException, FileNotFoundException etc
+for example IOException, FileNotFoundException etc.
  * UnChecked Exception: Exceptions that are generally due to logical errors and checked at the run time,
 for example NullPointerException, ArrayIndexOutOfBoundException etc
  */
@@ -21,7 +21,7 @@ fun calculate() {
     var a = 10
 
     try {
-        var sum = a / 0
+        val sum = a / 0
         println(sum)
     } catch (e: Exception) {
         println("$a can not divided by ZERO")
