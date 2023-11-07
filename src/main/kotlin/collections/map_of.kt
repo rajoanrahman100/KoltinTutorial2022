f6b6ce8f40ee93fa9c8296f8a9d3fd0595a2cec1
rajoanrahman100 <60685715+rajoanrahman100@users.noreply.github.com>
2023-11-07 13:29:02 +0600

    Update map_of.kt

--- src/main/kotlin/collections/map_of.kt
+++ src/main/kotlin/collections/map_of.kt
@@ -3,5 +3,9 @@ package collections
 fun main() {
     var nameMap= mapOf<Int,String>(1 to "New",2 to "View",3 to "Dart")
 
-
+    println("Map Entries : "+nameMap)
+ 
+    println("Map Keys: "+nameMap.keys )
+ 
+    println("Map Values: "+nameMap.values )
 }
