f01d226604bbb6ae01fc1b06dcc70d52bd01bb68
rajoanrahman100 <60685715+rajoanrahman100@users.noreply.github.com>
2023-11-07 13:30:03 +0600

    Update map_of.kt

--- src/main/kotlin/collections/map_of.kt
+++ src/main/kotlin/collections/map_of.kt
@@ -1,6 +1,10 @@
 package collections
 
 fun main() {
+
+    //Kotlin distinguishes between immutable and mutable maps. 
+    //Immutable maps created with mapOf() means these are read-only and mutable maps created with mutableMapOf() means we can perform read and write both.
+    
     var nameMap= mapOf<Int,String>(1 to "New",2 to "View",3 to "Dart")
 
     println("Map Entries : "+nameMap)
